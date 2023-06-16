cd147b42047676df5867a611d75ffb0bebebadca
Bruno <brunoomiguel10@hotmail.com>
2023-06-16 21:03:03 +0100

    Added function to server.c

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: brunolopes <brunolopes@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 09:51:51 by brunolopes        #+#    #+#             */
-/*   Updated: 2023/06/16 12:55:22 by brunolopes       ###   ########.fr       */
+/*   Updated: 2023/06/16 20:55:04 by brunolopes       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,22 +23,21 @@ int checkArgs(int ac, char *av)
 
 char sendMessage(pid_t pid, const char *str)
 {
-	int	i
 	int	bits;
 
-	i = 0;
-	while(str[i])
+	while(*str)
 	{
 		bits = 0;
 		while(bits != 8)
 		{
-			if((str[i] & (0x01 << bits)) != 0)
+			if((*str & (0x01 << bits)) != 0)
 				kill(pid, SIGUSR1);
 			else
 				kill(pid, SIGUSR2);
 			usleep(50);
 			bits++;
 		}
+		*str++;
 	}
 			
 }
@@ -48,7 +47,7 @@ int main(int argc, char **argv)
 	int pid;
 	if(checkArgs(argc, *argv[1]))
 	{
-		ft_printf("Wrong number of arguments or invalid PID;");
+		ft_printf("Wrong number of arguments or invalid PID");
 		return (0);
 	}
 	pid = ft_atoi(argv[1])
--- server.c
+++ server.c
@@ -6,14 +6,38 @@
 /*   By: brunolopes <brunolopes@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 09:52:21 by brunolopes        #+#    #+#             */
-/*   Updated: 2023/06/16 13:18:01 by brunolopes       ###   ########.fr       */
+/*   Updated: 2023/06/16 21:02:12 by brunolopes       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void handle_msg(int signal)
+{
+	static int	bit_count;
+	static unsigned char	c;
+
+	bit_count = -1;
+	if(bit_count < 0)
+		bit_count = 7;
+	if(signal == SIGUSR1)
+		c |= (1 << bits);
+	bit_count--;
+	if(bit_count && c)
+	{
+		ft_putchar_fd(c, 1);
+		c = NULL;
+	}
+}
 
 int main(void)
 {
-	
+	ft_printf("PID: %i\n", getpid());
+	ft_printf("Waiting on message");
+	while(true)
+	{
+		signal(SIGUSR1, handle_msg);
+		signal(SIGUSR2, handle_msg);
+	}
+	return (0);
 }
