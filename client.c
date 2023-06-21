ef299db1f08145fc562bd7c6fe370c597ab37e11
Bruno <brunoomiguel10@hotmail.com>
2023-06-21 15:09:20 +0100

    Fixed norm errors

--- client.c
+++ client.c
@@ -6,50 +6,50 @@
 /*   By: brunolopes <brunolopes@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 09:51:51 by brunolopes        #+#    #+#             */
-/*   Updated: 2023/06/21 15:03:46 by brunolopes       ###   ########.fr       */
+/*   Updated: 2023/06/21 15:08:54 by brunolopes       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int checkArgs(int ac)
+int	check_args(int ac)
 {
 	if (ac != 3)
 		return (1);
 	return (0);
 }
 
-void sendMessage(int pid, char *str)
+void	send_message(int pid, char *str)
 {
 	int	bits;
 
-	while(*str)
+	while (*str)
 	{
 		bits = 0;
-		while(bits != 8)
+		while (bits != 8)
 		{
-			if((*str & (0x01 << bits)) != 0)
-				kill(pid, SIGUSR1);
+			if ((*str & (0x01 << bits)) != 0)
+				kill (pid, SIGUSR1);
 			else
-				kill(pid, SIGUSR2);
-			usleep(50);
+				kill (pid, SIGUSR2);
+			usleep (50);
 			bits++;
 		}
 		str++;
 	}
-			
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int pid;
-	if(checkArgs(argc))
+	int	pid;
+
+	if (check_args (argc))
 	{
-		ft_printf("Wrong number of arguments");
+		ft_printf ("Wrong number of arguments");
 		return (1);
 	}
-	pid = ft_atoi(argv[1]);
-	sendMessage(pid, argv[2]);
-	sendMessage(pid, "\n");
+	pid = ft_atoi (argv[1]);
+	send_message (pid, argv[2]);
+	send_message (pid, "\n");
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,39 +6,39 @@
 /*   By: brunolopes <brunolopes@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 09:52:21 by brunolopes        #+#    #+#             */
-/*   Updated: 2023/06/21 15:04:03 by brunolopes       ###   ########.fr       */
+/*   Updated: 2023/06/21 15:07:04 by brunolopes       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void handle_msg(int signal)
+void	handle_msg(int signal)
 {
-	static int	bit_count = 0;
+	static int				bit_count = 0;
 	static unsigned char	c = 0;
 
-	if(signal == SIGUSR1)
+	if (signal == SIGUSR1)
 		c |= (0x01 << bit_count);
 	bit_count++;
-	if(bit_count == 8)
+	if (bit_count == 8)
 	{
-		ft_printf("%c", c);
+		ft_printf ("%c", c);
 		c = 0;
 		bit_count = 0;
 	}
 }
 
-int main(void)
+int	main(void)
 {
 	int	pid;
 
 	pid = getpid();
-	ft_printf("PID: %i\n", pid);
-	ft_printf("Waiting on message from client... \n");
-	while(true)
+	ft_printf ("PID: %i\n", pid);
+	ft_printf ("Waiting on message from client... \n");
+	while (true)
 	{
-		signal(SIGUSR1, handle_msg);
-		signal(SIGUSR2, handle_msg);
+		signal (SIGUSR1, handle_msg);
+		signal (SIGUSR2, handle_msg);
 	}
 	return (0);
 }
